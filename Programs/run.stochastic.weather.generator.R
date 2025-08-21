a4e572ac431bc4ac4da84b81f850df999f5a3f9a
Tung N <6590141+tungttnguyen@users.noreply.github.com>
2025-08-21 03:55:13 -0700

    Add parallel option to climate perturbation. Pre-compute several variables to improve function performance
    - `execute.simulations()` has parallel option that can be turned on;
    - move `jitter.samp` and `samp.cf` out of `quantile.mapping()` to avoid recalculate for each climate change scenario;
    - add a fallback option using `tryCatch` in `CC.scale()` in case `optim` fails;
    - use `data.table::fwrite` to speed up `create.delimited.outputs()` function.

--- Programs/run.stochastic.weather.generator.R
+++ Programs/run.stochastic.weather.generator.R
@@ -1,28 +1,36 @@
 
-rm(list=ls())
+# rm(list=ls())
 
-library(MASS) # Gamma fit
-library(evmix) # GPD fit
-library(eva) # GPD fit
-library(depmixS4) # HMMs/NHMMs fit
-library(markovchain) # HMMs/NHMMs fit
-library(rebmix) # split/WRs
-library(lpSolve) # lp optimization
-library(mvtnorm) # MVN
-library(lubridate) # dates
-library(tictoc) # run time
-library(moments) # computation
-library(abind) # computation
-#--------------------------------
-library(zoo) # (plot)
-library(fExtremes) # (plot)
-library(scales) # (plot)
-library(parallel) # (plot)
-library(proxy) # (plot)
-library(POT) # (plot) event-based computations
-library(extRemes) # (plot)
-library(ismev) # (plot)
-library(readxl) # output
+# Load required libraries -------------------------------------------------
+
+### Use pacman to automatically install missing libraries
+if (!require("pacman")) install.packages("pacman")
+library(pacman)
+
+p_load(MASS)        # Gamma fit
+p_load(lpSolve)     # lp optimization
+p_load(mvtnorm)     # MVN
+p_load(moments)     # computation
+p_load(abind)       # computation
+p_load(lubridate)   # dates
+p_load(depmixS4)    # HMMs/NHMMs fit
+p_load(markovchain) # HMMs/NHMMs fit
+p_load(rebmix)      # split/WRs
+p_load(evmix)       # GPD fit
+p_load(eva)         # GPD fit
+p_load(POT)         # (plot) event-based computations
+p_load(extRemes)    # (plot)
+p_load(ismev)       # (plot)
+p_load(fExtremes)   # (plot)
+p_load(parallel)    # (plot)
+p_load(zoo)         # (plot)
+p_load(proxy)       # (plot)
+p_load(scales)      # (plot)
+p_load(readxl)      # output
+
+start_time0 <- Sys.time()
+
+# Load config file --------------------------------------------------------
 
 source("./Programs/config.simulations.R") # config file
 
@@ -30,8 +38,9 @@ lst <- config.simulations() # call in configuration inputs
 for (i in 1:length(lst)) {assign(names(lst[i]), lst[[i]]) }; rm(lst)
 
 
-#*************************************
-#--- Weather Regimes Module ---#
+
+# Weather Regimes Module --------------------------------------------------
+
 #use provided WRs
 if (use.provided.WRs){
   final.NHMM.output <- readRDS('./Data/simulated.data.files/WRs.out/final.NHMM.non_param.output.rds')
@@ -52,30 +61,41 @@ if (use.provided.WRs){
 rm(final.NHMM.output) # for memory
 
 
-#*************************************
-#--- Weather Generation Module ---#
-execute.simulations()
-# done. #
+
+# Weather Generation Module -----------------------------------------------
+
+start_time1 <- Sys.time()
+execute.simulations(parallel = FALSE, number_of_cores = NULL)
+# execute.simulations(parallel = TRUE, number_of_cores = 2)
+Sys.time() - start_time1
+invisible(gc())
 
 
-# EXTRA #
+
+# EXTRA -------------------------------------------------------------------
+
 ### Below are auxiliary functions to do a list of tasks
-#*************************************
 # - create sample figures for selected scenario
 # - generate individual output files in tab or text delimited formats
 
 #this is the scenario (i.e., the row in ClimateChangeScenarios.csv) for which to make plots and write out the data as .csv files
 selected_scenario = 1
 
-#--- figures ---#
+## Figures -----------------------------------------------------------------
 # arguments are labels for x and y-axes
-start_time <- Sys.time()
+start_time2 <- Sys.time()
 create.figures.baselines.stacked(scenario = selected_scenario)
-Sys.time() - start_time
+Sys.time() - start_time2
 
+## Output csv files --------------------------------------------------------
 
-#--- outputs ---#
 # YYYY, MM, DD, P(mm), Tmax(C), Tmin(C) in .csv individual lat/lon file #
 # for simulated data #
+start_time3 <- Sys.time()
 create.delimited.outputs(scenario = selected_scenario)
+Sys.time() - start_time3
 
+end_time0 <- Sys.time() - start_time0
+end_time0
+
+print(paste("Total runtime:", round(end_time0, 2)))
